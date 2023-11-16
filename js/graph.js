b6ee74858397392922cb95357549e761ea34e5eb
Oleksii Sheldagaiev <furious3d@gmail.com>
2023-11-16 15:08:47 +0100

    extend graph functionality

--- js/graph.js
+++ js/graph.js
@@ -4,6 +4,16 @@ class Graph {
         this.segments = segments;
     }
 
+    getSegmentsByPoint(point) {
+        const segs = [];
+        this.segments.forEach((s) => {
+            if (s.includesPoint(point)) {
+                segs.push(s);
+            }
+        });
+        return segs;
+    }
+
     addPoint(point) {
         this.points.push(point);
     }
@@ -12,12 +22,29 @@ class Graph {
         this.segments.push(segment);
     }
 
+    removePoint(point) {
+        const segs = this.getSegmentsByPoint(point);
+        segs.forEach((s) => {
+            this.removeSegment(s);
+        });
+        this.points.splice(this.points.indexOf(point), 1);
+    }
+
+    removeSegment(seg) {
+        this.segments.splice(this.segments.indexOf(seg), 1);
+    }
+
     containsPoint(point) {
         return this.points.find((p) => p.equals(point));
     }
 
     containsSegment(segment) {
         return this.segments.find((s) => s.equals(segment));
+    }
+
+    clear() {
+        this.points.length = 0;
+        this.segments.length = 0;
     }
 
     draw(ctx) {
