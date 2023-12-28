20a2fdd9afb3904b5ec6cddecb8213226a362bea
Oleksii Sheldagaiev <furious3d@gmail.com>
2023-12-28 17:52:26 +0100

    create World class

---
+++ js/world.js
@@ -0,0 +1,25 @@
+class World {
+    constructor(graph, roadWidth = 100, roadRoundness = 6) {
+        this.graph = graph;
+        this.roadWidth = roadWidth;
+        this.roadRoundness = roadRoundness;
+
+        this.envelopes = [];
+
+        this.generate();
+    }
+
+    generate() {
+        this.envelopes.length = 0;
+
+        for (const seg of this.graph.segments) {
+            this.envelopes.push(new SegmentEnvelope(seg, this.roadWidth, this.roadRoundness));
+        }
+    }
+
+    render(ctx) {
+        this.envelopes.forEach((e) => {
+            e.draw(ctx);
+        });
+    }
+}
