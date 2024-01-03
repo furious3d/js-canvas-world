ca333d5724eedf5e0a2d167c90a09351b788b7e3
Oleksii Sheldagaiev <furious3d@gmail.com>
2024-01-03 17:32:36 +0100

    add new editor class

---
+++ js/editors/stop-editor.js
@@ -0,0 +1,19 @@
+class StopEditor {
+    constructor(viewport, world) {
+        this.viewport = viewport;
+        this.canvas = this.viewport.canvas;
+        this.ctx = this.canvas.getContext("2d");
+    }
+
+    enable() {
+
+    }
+
+    disable() {
+        
+    }
+
+    render() {
+
+    }
+}
